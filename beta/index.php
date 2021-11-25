8777fff5e3b918b04c3a9db6afdd0ce07442dc8e
lingan233 <71675451+lingan233@users.noreply.github.com>
2021-11-25 01:02:01 -0500

    Add files via upload

--- beta/index.php
+++ beta/index.php
@@ -6,11 +6,6 @@ include_once __DIR__ . '/header.php';
         <div class="search-area">
             <div class="dark-box">
                 <h1>Find a Team Recipe</h1>
-                <div class="search-bar">
-                    <input type="text" placeholder="Enter character names..." class="search-input">
-                    <button type="submit" class="search-btn"></button>
-                </div>
-
             </div>
         </div>
 
